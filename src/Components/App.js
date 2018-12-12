3cc75ad21517407b5ff3fa1791d0ed947f894d74
samueljamesfoster <samueljamesfoster@gmail.com>
2018-12-12 18:28:03 -0500

    Front end and basic js function

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
+//THis doesn't appear to work at all
+import jquery from 'jquery';
+window.$ = window.jQuery = jquery;
+require('foundation-sites');
 
-class App extends Component {
+// This is what it could look like with less rubbish colours and designs: https://whip-waiter.glitch.me
+class App extends Component { 
   render() {
     return (
-      <div className="App">
-        Alreet, you stupid fuckhead
+      <div className="grid-x">
+        <header className="cell">
+        
+          <section id="hero">
+            <div className="grid-container">
+              <div className="grid-x grid-padding-x align-center">
+                <div className="small-12">
+                  <h1>Card game</h1>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="deck">
+            <div className="grid-container">
+              <div className="grid-x grid-padding-x align-center">
+                <div className="small-12">
+                  <h2>This is the deck</h2>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </header>
+      
+        <main className="cell">
+
+          <section id="players">
+            <div className="grid-container">
+              <div className="grid-x grid-padding-x align-center">
+                <div className="small-12">
+                  <h2>These are the players</h2>
+                  <div className="grid-x grid-padding-x">  
+
+                      <div id="player"className="cell small-6">
+                        <h3>Player 1</h3>
+                      </div>
+
+                      <div id="ai" className="cell small-6">
+                        <h3>Player 2</h3>
+                      </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </main>
       </div>
     );
   }
