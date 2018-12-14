14941c493fdf5ffe4fa4b5b4ab4d888e2ffe1bed
James <jhodgson71@gmail.com>
2018-12-14 21:41:30 +0000

    Built out a grid in Material UI

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,65 +1,77 @@
 import React, { Component } from 'react';
-//THis doesn't appear to work at all
-import jquery from 'jquery';
-window.$ = window.jQuery = jquery;
-require('foundation-sites');
+import { Grid, Paper, Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 
-// This is what it could look like with less rubbish colours and designs: https://whip-waiter.glitch.me
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#eeeeee',
+    height: 800
+  },
+  header: {
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.primary.main,
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center'
+  },
+
+  section: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.secondary.main,
+    height: 300
+  },
+
+  // Text
+  title: {
+    fontSize: 20,
+    color: theme.palette.primary.main
+  },
+
+  subtitle: {
+    fontSize: 15,
+    color: theme.palette.secondary.main
+  }
+});
+
 class App extends Component { 
+
   render() {
+    const { classes } = this.props
+
     return (
-      <div className="grid-x">
-        <header className="cell">
-        
-          <section id="hero">
-            <div className="grid-container">
-              <div className="grid-x grid-padding-x align-center">
-                <div className="small-12">
-                  <h1>Card game</h1>
-                </div>
-              </div>
-            </div>
-          </section>
 
-          <section id="deck">
-            <div className="grid-container">
-              <div className="grid-x grid-padding-x align-center">
-                <div className="small-12">
-                  <h2>This is the deck</h2>
-                </div>
-              </div>
-            </div>
-          </section>
+          <Grid container justify="center" alignItems='flex-start' spacing={8} className={classes.root}>
 
-        </header>
-      
-        <main className="cell">
+            <Grid item xs={12}>
+              <Paper className={classes.header}>
+                <Typography className={classes.title}>Card Game</Typography>
+              </Paper>
+            </Grid>
+            
+            <Grid item xs={12} >
+              <Paper className={classes.section}>
+                <Typography className={classes.subtitle}>This would contain the main view</Typography>
+              </Paper>
+            </Grid>
+            
+            <Grid item xs={6}>
+              <Paper className={classes.section}>
+                <Typography className={classes.subtitle}>This would contain player 1 content</Typography>
+              </Paper>
+            </Grid>
+            
+            <Grid item xs={6}>
+              <Paper className={classes.section}>
+                <Typography className={classes.subtitle}>This would contain player 2 content</Typography>
+              </Paper>
+            </Grid>
 
-          <section id="players">
-            <div className="grid-container">
-              <div className="grid-x grid-padding-x align-center">
-                <div className="small-12">
-                  <h2>These are the players</h2>
-                  <div className="grid-x grid-padding-x">  
+          </Grid>
 
-                      <div id="player"className="cell small-6">
-                        <h3>Player 1</h3>
-                      </div>
-
-                      <div id="ai" className="cell small-6">
-                        <h3>Player 2</h3>
-                      </div>
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-
-        </main>
-      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
