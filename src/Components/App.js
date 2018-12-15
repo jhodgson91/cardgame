b1b28ccbde4410466a429470aa4c41e0c87e8de7
samueljamesfoster <samueljamesfoster@gmail.com>
2018-12-14 19:00:19 -0500

    2nd go and sorting out an stupid thing were I pulled the repo

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import styles from './App.css';
+import './App.css';
 import $ from 'jquery';
 import 'foundation-sites';
-
+//import { Grid, Paper, Typography } from '@material-ui/core'
+//import { withStyles } from '@material-ui/core/styles'
 
 // This is what it could look like with less rubbish colours and designs: https://whip-waiter.glitch.me
-class App extends Component { 
+
+class Game extends Component {
+  
   
   componentDidMount() {
     $(document).foundation();
   }
   
   render() {
-    return (
+    return(
       <div className="grid-x">
         <header className="cell">
-        
-          <section id="hero" className={styles['hero']}>
+
+          <section id="hero" className="hero">
             <div className="grid-container">
               <div className="grid-x grid-padding-x align-center">
                 <div className="small-12">
@@ -26,9 +28,57 @@ class App extends Component {
               </div>
             </div>
           </section>
-=======
-import { Grid, Paper, Typography } from '@material-ui/core'
-import { withStyles } from '@material-ui/core/styles'
+
+          <section id="deck">
+            <div className="grid-container">
+              <div className="grid-x grid-padding-x align-center">
+                <div className="small-12">
+                  <h2>This is the deck</h2>
+                  <div className="card"></div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </header>
+
+        <main className="cell">
+
+          <section id="players">
+            <div className="grid-container">
+              <div className="grid-x grid-padding-x align-center">
+                <div className="small-12">
+                  <h2>These are the players</h2>
+                  <div className="grid-x grid-padding-x text-center"> 
+
+                      <div id="player" className="cell small-6">
+                        <h3>Player 1</h3>
+                        <div className="card"></div>
+                        <p>Remaining:</p>
+                      </div>
+
+                      <div id="ai" className="cell small-6">
+                        <h3>Player 2</h3>
+                        <div className="card"></div>
+                        <p>Remaining:</p>
+                      </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+        </main>
+      </div>
+      
+    );
+  }
+  
+}
+
+export default Game;
+/*
 
 const styles = theme => ({
   root: {
@@ -56,8 +106,6 @@ const styles = theme => ({
     fontSize: 20,
     color: theme.palette.primary.main
   },
->>>>>>> 14941c493fdf5ffe4fa4b5b4ab4d888e2ffe1bed
-
   subtitle: {
     fontSize: 15,
     color: theme.palette.secondary.main
@@ -103,4 +151,4 @@ class App extends Component {
   }
 }
 
-export default withStyles(styles)(App);
+export default withStyles(styles)(App);*/
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import Game from './Components/App';
+//import App from './Components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Game />, document.getElementById('game'));
