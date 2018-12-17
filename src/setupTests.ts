5a2db33d89c59f664246dc83435b35724a90f2aa
samueljamesfoster <samueljamesfoster@gmail.com>
2018-12-17 18:07:03 -0500

    Added enzyme for component specific testing

---
+++ src/setupTests.ts
@@ -0,0 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+//Needs to be here because it's typescript
+export default undefined
