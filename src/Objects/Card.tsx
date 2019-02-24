b8935397e32c771dc5ff739fd19cf298dc918fd3
samueljamesfoster <samueljamesfoster@gmail.com>
2019-02-23 19:43:03 -0500

    Refactored card with typescript fixes

--- src/Objects/Card.tsx
+++ src/Objects/Card.tsx
@@ -2,56 +2,47 @@ import * as api from '../api'
 
 export interface CardData {
     image: string,
+    code: api.CardCode,
     value: api.CardValue,
-    suit: api.CardSuit,
-    code: api.CardCode
+    suit: api.CardSuit
 }
 
 class Card {
-    data: CardData = {
-      image: '',
-      value: '',
-      suit: '',
-      code: ''
-    }
+    _image: string
+    _code: api.CardCode
+    _value: api.CardValueCode
+    _suit: api.CardSuitCode
 
     constructor(data: CardData) {
         this._image = data.image
-        this._value = data.value
-        this._suit = data.suit
         this._code = data.code
+        this._value = data.code[0] as api.CardValueCode
+        this._suit = data.code[1] as api.CardSuitCode
     }
 
     valueOf() {
         return this.numericValue
     }
-  
     toString() {
         return `${this.value} OF ${this.suit}`
     }
-  
     get isPictureCard() {
         return this.value === api.CardValue.JACK
             || this.value === api.CardValue.QUEEN
             || this.value === api.CardValue.KING
     }
-    
-    get image() {
-        return this._image;
-    }
-  
     get numericValue() {
         return api.getCardValue(this)
     }
-  
+    get image() {
+        return this._image;
+    }
     get value() {
         return api.CardValueMap[this._value];
     }
-  
     get suit() {
         return api.CardSuitMap[this._suit];
     }
-    
     get code() {
         return this._code;
     }
