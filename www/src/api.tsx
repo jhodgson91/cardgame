a05a35c9d3a4abd601bad8b31edc1c9a4ce90ea1
jhodgson91 <jhodgson71@gmail.com>
2019-04-10 21:56:05 +0100

    Added API types

--- www/src/api.tsx
+++ www/src/api.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import Deck from "./Objects/Deck";
 
 export const url = "https://deckofcardsapi.com/api/"
@@ -8,4 +8,62 @@ export async function getDeck({ deck_id = "new", shuffled = true } = {}) {
         .then(response => {
             return response.data.success ? new Deck(response.data) : undefined
         })
+}
+
+export type CardSuit = "H" | "D" | "S" | "C";
+export type CardValue = "A" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0" | "J" | "Q" | "K";
+export type CardCode = "AS" | "2S" | "3S" | "4S" | "5S" | "6S" | "7S" | "8S" | "9S" | "0S" | "JS" | "QS" | "KS"
+    | "AD" | "2D" | "3D" | "4D" | "5D" | "6D" | "7D" | "8D" | "9D" | "0D" | "JD" | "QD" | "KD"
+    | "AC" | "2C" | "3C" | "4C" | "5C" | "6C" | "7C" | "8C" | "9C" | "0C" | "JC" | "QC" | "KC"
+    | "AH" | "2H" | "3H" | "4H" | "5H" | "6H" | "7H" | "8H" | "9H" | "0H" | "JH" | "QH" | "KH";
+
+export type PileResponse = { [name: string]: CardCode[] };
+export type GameResponse = {
+    id: string,
+    piles: PileResponse,
+}
+
+export type CardSelection =
+    { all: boolean }
+    | { top: number }
+    | { bottom: number }
+    | { random: number }
+    | { cards: Card[] }
+    | { filter: { suits?: CardSuit[], values?: CardValue[] } };
+
+export class Card {
+    code: CardCode;
+
+    constructor(code: CardCode) {
+        this.code = code;
+    }
+
+    get value(): CardSuit {
+        return (this.code as string)[0] as CardSuit;
+    }
+    get suit(): CardSuit {
+        return (this.code as string)[1] as CardSuit;
+    }
+}
+
+export type Piles = { [name: string]: Card[] }
+
+export class Game {
+    id: string;
+    piles: Piles;
+
+    constructor(data: GameResponse) {
+        this.id = data.id;
+        this.piles = {};
+        Object.keys(data.piles).map((key, i) => {
+            this.piles[key] = data.piles[key].map((code: CardCode) => new Card(code));
+        })
+    }
+}
+
+export const test_url = "/api/game";
+export async function test(): Promise<Game> {
+    return axios.post(test_url + "/new").then((response: AxiosResponse<GameResponse>) => {
+        return new Game(response.data);
+    })
 }
--- www/src/index.tsx
+++ www/src/index.tsx
@@ -2,4 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Game from './Components/Game'
 
+import * as api from './api';
+
+async function newapi_test() {
+    let g = await api.test();
+    console.log(g.piles["deck"][0].value);
+    console.log(g.piles["deck"][0].suit);
+}
+
+newapi_test();
 ReactDOM.render(<Game />, document.getElementById('game'));
